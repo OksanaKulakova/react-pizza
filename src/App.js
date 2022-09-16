7bb0bff5d41eb4e075934f91c0e42c0a6b7bc793
Оксана <oksana@cobalt42.ru>
2022-09-16 14:23:10 +0500

    Рендер списка пицц

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './scss/app.scss';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock.jsx';
+import PizzaBlock from './components/PizzaBlock';
+
+import pizzas from './assets/pizzas.json';
 
 function App() {
   return (
@@ -17,7 +19,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock />
+            {pizzas.map((pizza, i) => (
+              <PizzaBlock key={i} {...pizza} />
+            ))}
           </div>
         </div>
       </div>
